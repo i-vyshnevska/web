b6e32a638560fdd85cd1a779ba04a65e8f54d9ab
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-08-20 14:56:12 +0300

    [ADD] patch field to add size attr

--- web_text_count_field/patch.py
+++ web_text_count_field/patch.py
@@ -5,22 +5,27 @@ import logging
 
 import odoo
 from odoo.tools import pycompat
+from operator import attrgetter
 
 logger = logging.getLogger(__name__)
 
 
-def convert_to_column(self, value, record, values=None):
+def Text__convert_to_column(self, value, record, values=None, validate=True):
     if value is None or value is False:
         return None
-    # we need to convert the string to a unicode object to be able
-    # to evaluate its length (and possibly truncate it) reliably
-    return pycompat.to_text(value)[:self.size]
+    if  hasattr(self, 'size') and self.size != False:
+        return pycompat.to_text(value)[:self.size]
+    else:
+        return self.orig_convert_to_column(value, record, values, validate)
 
 
-def convert_to_cache(self, value, record, validate=True):
+def Text__convert_to_cache(self, value, record, validate=True):
     if value is None or value is False:
         return False
-    return pycompat.to_text(value)[:self.size]
+    if  hasattr(self, 'size') and self.size != False:
+        return pycompat.to_text(value)[:self.size]
+    else:
+        return self.orig_convert_to_cache(value, record, validate)
 
 
 def patch_fields():
@@ -29,5 +34,10 @@ def patch_fields():
     slots = Text._slots
     slots['size'] = None
     setattr(Text, '_slots', slots)
-    setattr(Text, 'convert_to_column', convert_to_column)
-    setattr(Text, 'convert_to_column', convert_to_cache)
+    setattr(Text, '_related_size', property(attrgetter('size')))
+    Text.orig_convert_to_column = Text.convert_to_column
+    Text.convert_to_column = Text__convert_to_column
+    
+    Text.orig_convert_to_cache = Text.convert_to_cache
+    Text.convert_to_cache = Text__convert_to_cache
+
